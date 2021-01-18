30bd01e78c497da67d7f01688f6416b8852a0743
Dima Bond <neverpooh@gmail.com>
2021-01-18 19:30:33 +0200

    Updates

---
+++ precache-manifest.31196cbbd466fb5aecd0156417540b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aaae89805cd70a3e101f113e82666f7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "06c5e65b72248a849130",
+    "url": "./static/css/main.b2aeec7e.chunk.css"
+  },
+  {
+    "revision": "b5ebe3227a36fe6a4ae7",
+    "url": "./static/js/2.c3d77166.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.c3d77166.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c5e65b72248a849130",
+    "url": "./static/js/main.a3b1ce1c.chunk.js"
+  },
+  {
+    "revision": "f861d31e3669d4c8fef0",
+    "url": "./static/js/runtime-main.c970f6dd.js"
+  }
+]);
